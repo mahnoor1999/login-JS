ef3b3908bfb8becebd761fab695fed4bb630db53
mahnoor1999 <mahnoornaveed74@gmail.com>
2020-12-16 10:49:47 +0500

    add action attribute

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
-
 function email_check() {
-    let correctEmail = document.querySelector("#email").value
-    console.log(correctEmail);
-    let myEmail = "mahnoor@gmail.com"
-    console.log(myEmail);
-    if (correctEmail == myEmail) {
-        window.location.href = "password.html";
-    }
-    else {
-        let errorline = "Couldn't find Your Google Account"
-        document.querySelector("#email-error").innerHTML = errorline;
-
-
-    }
+  let inputEmail = document.querySelector("#email").value;
+  let myEmail = "mahnoor@gmail.com";
+  if (inputEmail == myEmail) {
+    return true;
+  } else {
+    let errorline = "Couldn't find Your Google Account";
+    document.querySelector("#email-error").innerHTML = errorline;
+    return false;
+  }
 }
 
 // password
-function password_checked() {
-    let correctPassword = document.querySelector("#password").value
-    console.log(correctPassword);
-    let myPassword = "mahnoor123"
-    console.log(myPassword);
-    if (correctPassword == myPassword) {
-        console.log("hey babes")
-        window.location.href = "welcome.html";
-    }
-    else {
-        let errorline = "Wrong password. Try again or click Forgot password to reset it."
-        document.querySelector("#password-error").innerHTML = errorline;
-
-
-    }
+function password_check() {
+  let inputPassword = document.querySelector("#password").value;
+  let myPassword = "mahnoor123";
+  if (inputPassword == myPassword) {
+   return true;
+  } else {
+    let errorline =
+      "Wrong password. Try again or click Forgot password to reset it.";
+    document.querySelector("#password-error").innerHTML = errorline;
+    return false;
+  }
 }
